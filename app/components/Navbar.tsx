4ea53045522b2fd1f68a79f88600fdacf725e869
Noah D'Arcy <noahdarcy.dev@gmail.com>
2024-06-09 02:06:02 -0400

    init commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="w-screen py-[5.2rem] pl-[6rem]">
+      <img src="./icons/portfolio-logo.svg" alt="Portfolio Logo" />
+    </nav>
+  );
+}
